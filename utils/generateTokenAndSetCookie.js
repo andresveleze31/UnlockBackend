9b3b188b97622ffbad1cf30dc337f92106ee416e
Andres Velez <andresvelezecheverry@gmail.com>
2024-11-19 15:35:55 -0500

    Changing Cookies

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -9,7 +9,7 @@ export const generateTokenAndSetCookie = (res, companieId) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV==="production",
-        sameSite: "strict",
+        sameSite: "None",
         maxAge: 7*24*60*60*1000,
 
     })
